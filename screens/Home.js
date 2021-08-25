be12844645079b14a4e084821be35fbb4926d047
TRAN_QUANG_DO <do.tranquang@neo-career.co.jp>
2021-08-26 01:48:55 +0900

    edit Home

--- screens/Home.js
+++ screens/Home.js
@@ -35,7 +35,7 @@ const Home = ({ navigation }) => {
         pageNo: 341,
         author: "Jasmine Warga",
         genre: [
-            "Romance", "Adventure", "Drama"
+            "LangMan", "PhieuLuu", "Kich"
         ],
         readed: "12k",
         description: "Jude never thought she’d be leaving her beloved older brother and father behind, all the way across the ocean in Syria. But when things in her hometown start becoming volatile, Jude and her mother are sent to live in Cincinnati with relatives. At first, everything in America seems too fast and too loud. The American movies that Jude has always loved haven’t quite prepared her for starting school in the US—and her new label of 'Middle Eastern,' an identity she’s never known before. But this life also brings unexpected surprises—there are new friends, a whole new family, and a school musical that Jude might just try out for. Maybe America, too, is a place where Jude can be seen as she really is.",
@@ -52,7 +52,7 @@ const Home = ({ navigation }) => {
         pageNo: 272,
         author: "Seith Fried",
         genre: [
-            "Adventure", "Drama"
+            "PhieuLuu", "Kich"
         ],
         readed: "13k",
         description: "In Metropolis, the gleaming city of tomorrow, the dream of the great American city has been achieved. But all that is about to change, unless a neurotic, rule-following bureaucrat and an irreverent, freewheeling artificial intelligence can save the city from a mysterious terrorist plot that threatens its very existence. Henry Thompson has dedicated his life to improving America's infrastructure as a proud employee of the United States Municipal Survey. So when the agency comes under attack, he dutifully accepts his unexpected mission to visit Metropolis looking for answers. But his plans to investigate quietly, quickly, and carefully are interrupted by his new partner: a day-drinking know-it-all named OWEN, who also turns out to be the projected embodiment of the agency's supercomputer. Soon, Henry and OWEN are fighting to save not only their own lives and those of the city's millions of inhabitants, but also the soul of Metropolis. The Municipalists is a thrilling, funny, and touching adventure story, a tour-de-force of imagination that trenchantly explores our relationships to the cities around us and the technologies guiding us into the future.",
@@ -69,7 +69,7 @@ const Home = ({ navigation }) => {
         pageNo: 110,
         author: "Ana C Bouvier",
         genre: [
-            "Drama", "Adventure", "Romance"
+            "Kich", "PhieuLuu", "LangMan"
         ],
         readed: "13k",
         description: "This sketchbook for kids is the perfect tool to improve your drawing skills! Designed to encourage kids around the world to express their uniqueness through drawing, sketching or doodling, this sketch book is filled with 110 high quality blank pages for creations. Add some fun markers, crayons, and art supplies and you have the perfect, easy gift for kids!",
@@ -110,14 +110,16 @@ const Home = ({ navigation }) => {
             id: 2,
             categoryName: "Moi nhat",
             books: [
-                bookTheMetropolis
+                bookTheMetropolis,
+                bookOtherWordsForHome,
+                bookTheTinyDragon,
             ]
         },
         {
             id: 3,
             categoryName: "Sap co",
             books: [
-                bookTheTinyDragon
+                bookTheTinyDragon,
             ]
         },
     ]
@@ -387,8 +389,8 @@ const Home = ({ navigation }) => {
                         <View style={{ flex: 1, marginLeft: SIZES.radius }}>
                             {/* Book name and author */}
                             <View>
-                                <Text style={{ paddingRight: SIZES.padding, ...FONTS.h2, color: COLORS.white }}>{item.bookName}</Text>
-                                <Text style={{ ...FONTS.h3, color: COLORS.lightGray4 }}>{item.author}</Text>
+                                <Text style={{ paddingRight: SIZES.padding, ...FONTS.h4, color: COLORS.white }}>{item.bookName}</Text>
+                                <Text style={{ ...FONTS.h4, color: COLORS.lightGray4 }}>{item.author}</Text>
                             </View>
 
                             {/* Book Info */}
@@ -419,21 +421,21 @@ const Home = ({ navigation }) => {
                             {/* Genre */}
                             <View style={{ flexDirection: 'row', marginTop: SIZES.base }}>
                                 {
-                                    item.genre.includes("Adventure") &&
+                                    item.genre.includes("PhieuLuu") &&
                                     <View style={{ justifyContent: 'center', alignItems: 'center', padding: SIZES.base, marginRight: SIZES.base, backgroundColor: COLORS.lightGray3, height: 40, borderRadius: SIZES.radius }}>
-                                        <Text style={{ ...FONTS.body4, color: COLORS.lightGreen }}>Adventure</Text>
+                                        <Text style={{ ...FONTS.body4, color: COLORS.lightGreen }}>PhieuLuu</Text>
                                     </View>
                                 }
                                 {
-                                    item.genre.includes("Romance") &&
+                                    item.genre.includes("LangMan") &&
                                     <View style={{ justifyContent: 'center', alignItems: 'center', padding: SIZES.base, marginRight: SIZES.base, backgroundColor: COLORS.lightGray3, height: 40, borderRadius: SIZES.radius }}>
-                                        <Text style={{ ...FONTS.body4, color: COLORS.lightRed }}>Romance</Text>
+                                        <Text style={{ ...FONTS.body4, color: COLORS.lightRed }}>LangMan</Text>
                                     </View>
                                 }
                                 {
-                                    item.genre.includes("Drama") &&
+                                    item.genre.includes("Kich") &&
                                     <View style={{ justifyContent: 'center', alignItems: 'center', padding: SIZES.base, marginRight: SIZES.base, backgroundColor: COLORS.lightGray3, height: 40, borderRadius: SIZES.radius }}>
-                                        <Text style={{ ...FONTS.body4, color: COLORS.lightBlue }}>Drama</Text>
+                                        <Text style={{ ...FONTS.body4, color: COLORS.lightBlue }}>Kich</Text>
                                     </View>
                                 }
                             </View>
@@ -466,6 +468,7 @@ const Home = ({ navigation }) => {
                     renderItem={renderItem}
                     keyExtractor={item => `${item.id}`}
                     showsVerticalScrollIndicator={false}
+                    horizontal
                 />
             </View>
         )
@@ -482,19 +485,10 @@ const Home = ({ navigation }) => {
             {/* Body Section */}
             <ScrollView style={{ marginTop: SIZES.radius }}>
                 {/* Books Section */}
-                <View>
-                    {renderMyBookSection(myBooksData)}
-                </View>
-
+                {renderMyBookSection(myBooksData)}
                 {/* Categories Section */}
-                <View style={{ marginTop: SIZES.padding }}>
-                    <View>
-                        {renderCategoryHeader()}
-                    </View>
-                    <View>
-                        {renderCategoryData()}
-                    </View>
-                </View>
+                {renderCategoryHeader()}
+                {renderCategoryData()}
             </ScrollView>
         </SafeAreaView>
     )
